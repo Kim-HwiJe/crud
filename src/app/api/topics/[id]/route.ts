6842e5de6cdefb82186ebd057328d12e015e1010
Kim-Hwije <hwije207@gmail.com>
2025-10-29 10:11:22 +0900

    1st

--- src/app/api/topics/[id]/route.ts
+++ src/app/api/topics/[id]/route.ts
@@ -4,15 +4,17 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  context: { params: { id: string } }
 ) {
   try {
-    const { id } = params
+    const { id } = context.params
     await connectMongoDB()
     const topic = await Topic.findOne({ _id: id })
+
     if (!topic) {
       return NextResponse.json({ message: 'Topic not found' }, { status: 404 })
     }
+
     return NextResponse.json({ topic }, { status: 200 })
   } catch (error) {
     console.error('Error in GET /api/topics/[id]:', error)
@@ -25,27 +27,31 @@ export async function GET(
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  context: { params: { id: string } }
 ) {
   try {
-    const { id } = params
+    const { id } = context.params
     const { newTitle: title, newDescription: description } =
       await request.json()
+
     if (!title || !description) {
       return NextResponse.json(
         { message: 'Title and description are required' },
         { status: 400 }
       )
     }
+
     await connectMongoDB()
     const updatedTopic = await Topic.findByIdAndUpdate(
       id,
       { title, description },
       { new: true }
     )
+
     if (!updatedTopic) {
       return NextResponse.json({ message: 'Topic not found' }, { status: 404 })
     }
+
     return NextResponse.json(
       { message: 'Topic updated', topic: updatedTopic },
       { status: 200 }
--- src/app/api/topics/route.ts
+++ src/app/api/topics/route.ts
@@ -2,52 +2,65 @@ import connectMongoDB from '@/libs/mongodb'
 import Topic from '@/models/topic'
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(
-  request: NextRequest,
-  context: { params: { id: string } }
-) {
+export async function POST(request: NextRequest) {
   try {
-    const { id } = context.params
+    const { title, description } = await request.json()
+
+    if (!title || !description) {
+      return NextResponse.json(
+        { message: 'Title and description are required' },
+        { status: 400 }
+      )
+    }
+
     await connectMongoDB()
-    const topic = await Topic.findById(id)
-    if (!topic) {
+    await Topic.create({ title, description })
+
+    return NextResponse.json({ message: 'Topic created' }, { status: 201 })
+  } catch (error) {
+    console.error('Error in POST /api/topics:', error)
+    return NextResponse.json(
+      { message: 'Internal Server Error' },
+      { status: 500 }
+    )
+  }
+}
+
+export async function GET() {
+  try {
+    await connectMongoDB()
+    const topics = await Topic.find()
+    return NextResponse.json({ topics }, { status: 200 })
+  } catch (error) {
+    console.error('Error in GET /api/topics:', error)
+    return NextResponse.json(
+      { message: 'Internal Server Error' },
+      { status: 500 }
+    )
+  }
+}
+
+export async function DELETE(request: NextRequest) {
+  try {
+    const id = request.nextUrl.searchParams.get('id')
+
+    if (!id) {
+      return NextResponse.json({ message: 'ID is required' }, { status: 400 })
+    }
+
+    await connectMongoDB()
+    const deletedTopic = await Topic.findByIdAndDelete(id)
+
+    if (!deletedTopic) {
       return NextResponse.json({ message: 'Topic not found' }, { status: 404 })
     }
-    return NextResponse.json({ topic }, { status: 200 })
-  } catch (error) {
-    console.error('GET /api/topics/[id] Error:', error)
-    return NextResponse.json({ message: 'Error' }, { status: 500 })
-  }
-}
 
-export async function PUT(
-  request: NextRequest,
-  context: { params: { id: string } }
-) {
-  try {
-    const { id } = context.params
-    const { newTitle: title, newDescription: description } =
-      await request.json()
-    await connectMongoDB()
-    await Topic.findByIdAndUpdate(id, { title, description })
-    return NextResponse.json({ message: 'Topic updated' }, { status: 200 })
-  } catch (error) {
-    console.error('PUT /api/topics/[id] Error:', error)
-    return NextResponse.json({ message: 'Error' }, { status: 500 })
-  }
-}
-
-export async function DELETE(
-  request: NextRequest,
-  context: { params: { id: string } }
-) {
-  try {
-    const { id } = context.params
-    await connectMongoDB()
-    await Topic.findByIdAndDelete(id)
     return NextResponse.json({ message: 'Topic deleted' }, { status: 200 })
   } catch (error) {
-    console.error('DELETE /api/topics/[id] Error:', error)
-    return NextResponse.json({ message: 'Error' }, { status: 500 })
+    console.error('Error in DELETE /api/topics:', error)
+    return NextResponse.json(
+      { message: 'Internal Server Error' },
+      { status: 500 }
+    )
   }
 }
