e6300db2e40c562d0c6a397911f6e65ad6762239
Kim-Hwije <hwije207@gmail.com>
2025-11-26 08:36:21 +0900

    2nd

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,7 @@
-export { auth as middleware } from '@/auth'
+export { auth as middleware } from "@/auth";
 
 export const config = {
   matcher: [
-    // 🔥 인증 제외할 경로 (로그 저장 / 유저 등록 API)
-    '/((?!api/log|api/user-auth|_next/static|_next/image|favicon.ico).*)',
+    "/((?!api/log|api/user-auth|api/auth|_next|favicon.ico).*)",
   ],
-}
+};
